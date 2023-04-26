d1eee2dc2b29c0cfe1b63e36f82f2702e5be7d6b
Franklin Briones <franklin@Franklins-MBP.attlocal.net>
2023-04-26 14:32:54 -0500

    add license key that generates

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import {v4 as uuidv4} from 'uuid';
+
 
 export default function handler(req, res) {
 
-    res.status(200).json({ name: 'Hi Paddle' })
+    res.status(200).json({uuid: uuidv4() })
 }
