a1d6d2a7793a249534dc6c627e4609d93ced985e
Franklin Briones <franklindbriones@gmail.com>
2023-05-05 12:34:18 -0500

    fix no-cors issue

--- pages/api/paylink.js
+++ pages/api/paylink.js
@@ -1,3 +1,33 @@
+import Cors from 'cors'
+
+// Initializing the cors middleware
+const cors = Cors({
+  methods: ['GET', 'HEAD'],
+})
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+
+      return resolve(result)
+    })
+  })
+}
+
+async function handler(req, res) {
+  // Run the middleware
+  await runMiddleware(req, res, cors)
+
+  // Rest of the API logic
+  res.json({ message: 'Hello Everyone!' })
+}
+
+export default handler
 
 
 
@@ -19,7 +49,8 @@ export async function payLink(unitQuantity) {
     let myInit = {
         method: "POST",
         headers: {
-            "Access-Control-Allow-Origin": "*"
+            "Access-Control-Allow-Origin": "*",
+            "RequestMode": "no-cors"
         },
         body: {
             "vendor_id" : process.env.VENDOR_ID,
